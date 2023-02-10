f8ba48067adf91996bc0e04ddb5ce5ad4107acb2
CharlieFan <charlie329@163.com>
2023-02-10 18:00:06 +0800

    Dockerfile scons reports glMapBufferOES not defined

--- docker_scripts/build_openpilot.sh
+++ docker_scripts/build_openpilot.sh
@@ -4,13 +4,26 @@ set -e
 # Inheriting some environment variables
 source ./tools/openpilot_env.sh
 
-# Do some random stuff
-echo "Looking form toml files"
-find . -name "*.toml"
+# Since openpilot_env.sh did not add shims to the PATH, we do it here
+export PATH=${PYENV_ROOT}/shims:${PATH}
+
+TF="/tmp/tmp.txt"
+touch ${TF}
+echo "Current python version $(python -V)" >> ${TF}
+echo "Current python location $(which python)" >> ${TF}
+#echo "ldconfig can find the following:\n$(ldconfig -v)" >> ${TF}
+
+ln -s ${PYENV_ROOT}/versions/3.8.10/lib/libpython3.8.so /usr/lib/aarch64-linux-gnu/libpython3.8.so
 
 # Start building
 echo "poetry starts a new shell and activates virtual env"
 poetry shell
 
-echo "Start building via scons"
-scons -u -j$(nproc)
+# play with poetry
+echo "=================================================== After poetry shell" >> ${TF}
+#echo "ldconfig can find the following:" >> ${TF}
+#echo $(ldconfig -v) >> ${TF}
+echo "poetry has installed the following packages:" >> ${TF}
+echo $(poetry show) >> ${TF}
+
+#scons -u -j$(nproc)
