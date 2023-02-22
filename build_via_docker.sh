a9c659c7228f44fe2e735461d66fcd16534f31cc
CharlieFan <charlie329@163.com>
2023-02-22 13:05:52 +0800

    Build filesystem

--- build_via_docker.sh
+++ build_via_docker.sh
@@ -40,3 +40,61 @@ echo "Building image"
 export DOCKER_BUILDKIT=1
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker build -f Dockerfile -t ok8mp-ubuntu-builder $DIR
+
+if [ "$1" == "buildfs" ]; then
+
+BUILD_DIR="$DIR/build"
+OUTPUT_DIR="$DIR/output"
+
+ROOTFS_DIR="$BUILD_DIR/ok8mp-rootfs"
+ROOTFS_IMAGE="$BUILD_DIR/system.img.raw"
+ROOTFS_IMAGE_SIZE=10G
+SPARSE_IMAGE="$BUILD_DIR/system.img"
+
+# Create temp dir if non-existent
+mkdir -p $BUILD_DIR $OUTPUT_DIR
+
+# Create filesystem ext4 image
+echo "Creating empty filesystem"
+fallocate -l $ROOTFS_IMAGE_SIZE $ROOTFS_IMAGE
+mkfs.ext4 $ROOTFS_IMAGE > /dev/null
+
+# Mount filesystem
+echo "Mounting empty filesystem"
+mkdir -p $ROOTFS_DIR
+sudo umount -l $ROOTFS_DIR > /dev/null || true
+sudo mount $ROOTFS_IMAGE $ROOTFS_DIR
+
+# Extract image
+echo "Extracting docker image"
+CONTAINER_ID=$(docker container create --entrypoint /bin/bash ok8mp-ubuntu-builder:latest)
+docker container export -o $BUILD_DIR/filesystem.tar $CONTAINER_ID
+docker container rm $CONTAINER_ID > /dev/null
+echo "Extract to build/filesystem.tar ..."
+cd $ROOTFS_DIR
+sudo tar -xf $BUILD_DIR/filesystem.tar > /dev/null
+
+# Add hostname and hosts. This cannot be done in the docker container...
+echo "Setting network stuff"
+HOST=tici
+sudo bash -c "echo $HOST > etc/hostname"
+sudo bash -c "echo \"127.0.0.1    localhost.localdomain localhost\" > etc/hosts"
+sudo bash -c "echo \"127.0.0.1    $HOST\" >> etc/hosts"
+
+# Fix resolv config
+sudo bash -c "ln -sf /run/systemd/resolve/stub-resolv.conf etc/resolv.conf"
+
+# Write build info
+DATETIME=$(date '+%Y-%m-%dT%H:%M:%S')
+GIT_HASH=$(git --git-dir=$DIR/.git rev-parse HEAD)
+sudo bash -c "printf \"$GIT_HASH\n$DATETIME\" > BUILD"
+
+cd $DIR
+
+# Unmount image
+echo "Unmount filesystem"
+sudo umount -l $ROOTFS_DIR
+
+echo "Done!"
+
+fi
